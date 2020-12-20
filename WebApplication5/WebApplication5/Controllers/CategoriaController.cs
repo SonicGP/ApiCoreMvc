3be199d921dc2d7e5b0207d8a3c49ec8074e182c
Anderson Gomes <gomesa80@yahoo.com.br>
2020-12-20 06:36:15 -0300

    Projeto ApiCoreMvc

---
+++ WebApplication5/WebApplication5/Controllers/CategoriaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Model;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AplicationContext context;
+
+        public CategoriaController(AplicationContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Categoria> Get()
+        {
+            return context.Categorias.ToList();
+        }
+
+        [HttpGet("{id}", Name = "categoriaCriada")]
+        public IActionResult GetById( int id)
+        {
+            var cat = context.Categorias.FirstOrDefault(x=> x.Id == id);
+            if(cat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cat);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categorias.Add(categoria);
+                context.SaveChanges();
+                return new CreatedAtRouteResult("categoriaCriada", new { id = categoria.Id}, categoria);
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ WebApplication5/WebApplication5/Model/AplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Model
+{
+    public class AplicationContext : DbContext
+    {
+        public AplicationContext(DbContextOptions<AplicationContext>options): base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        
+
+        /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         {
+             base.OnConfiguring(optionsBuilder);
+         }*/
+    }
+}
